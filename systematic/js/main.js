23f704d0ad7e8f9d7d796df294b86ee7ef66be46
Arif Mia <93834911+erparif@users.noreply.github.com>
2022-06-01 19:22:30 +0600

    Add files via upload

---
+++ systematic/js/main.js
@@ -0,0 +1,9 @@
+// Bx Slider
+
+$(document).ready(function(){
+  $('.slider_area').bxSlider();
+  $('#main_menu').meanmenu({
+    meanMenuContaier:"#mobile_menu",
+    meanScreenWidth:991,
+  });
+});
